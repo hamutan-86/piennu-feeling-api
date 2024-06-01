c19a79cc7b774db8c57f802ea24b22d6284a3535
hamutan-86 <158189349+hamutan-86@users.noreply.github.com>
2024-06-01 13:27:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,86 @@
+// import express from 'express';
 const express = require("express");
+const fs = require('fs');
+
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get('/', (req, res) => {
+  res.send('Piennu Feeling API');
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/getCount', (req, res) => {
+  fetch("https://object.piennu777.jp/feelings/api/getcount.php", {method: "GET", headers: {
+    "accept": "*/*",
+    "origin": "https://object.piennu777.jp",
+    "referer": "https://object.piennu777.jp/feelings/",
+    "accept-encoding": "gzip, deflate, br",
+    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1",
+    "content-length": "0",
+    "accept-language": "en-US,en;q=0.9"
+  }},).then(data => data.text()).then((resp) => {
+    res.status(200).send(resp);
+  })
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.post('/increment/createTask', (req, res) => {
+  if (!req.body.amount){
+    res.status(400).json({"error": "Form body Invalid"})
+    return false;
+  } else if (typeof req.body.amount != "number"){
+    res.status(400).json({"error": "Form body Invalid"})
+    return false;
+  }
+  let count = 0;
+  var taskId = Math.floor(Math.random()* 100000000)
+  let jsonObject = JSON.parse(fs.readFileSync('./tasks.json', 'utf8'));
+  jsonObject[taskId.toString()] = "pending";
+  fs.writeFileSync('./tasks.json', JSON.stringify(jsonObject));
+  res.status(200).json({"taskId": taskId})
+  while (true) {
+    fetch("https://object.piennu777.jp/feelings/api/increment.php", {method: "POST", headers: {
+        "accept": "*/*",
+        "origin": "https://object.piennu777.jp",
+        "referer": "https://object.piennu777.jp/feelings/",
+        "accept-encoding": "gzip, deflate, br",
+        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1",
+        "content-length": "0",
+        "accept-language": "en-US,en;q=0.9"
+      }},).then((data) => {})
+    count++
+    if (count == req.body.amount){
+      break;
+    }
+  }
+  jsonObject[taskId.toString()] = "success";
+  fs.writeFileSync('./tasks.json', JSON.stringify(jsonObject));
+  return true;
+});
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.post('/increment/getTaskResult', (req, res) => {
+  if (!req.body.taskId){
+    res.status(400).json({"error": "Form body Invalid"})
+    return false;
+  } else if (typeof req.body.taskId != "number" && typeof req.body.taskId != "string"){
+    res.status(400).json({"error": "Form body Invalid"})
+    return false;
+  }
+  let taskId = req.body.taskId;
+  if (typeof taskId === "number"){
+    taskId = taskId.toString();
+  }
+  let jsonObject = JSON.parse(fs.readFileSync('./tasks.json', 'utf8'));
+  for (var obj in jsonObject){
+    if (obj === taskId){
+      res.status(200).json({"status": jsonObject[taskId]})
+      return true;
+    }
+    res.status(400).json({"error": "Task not exists"})
+    return false;
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Express server initialized');
+});
